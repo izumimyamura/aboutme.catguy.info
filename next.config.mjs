e9bb6f99ba317406e4ed3f975fddd75771b68623
THECATGUY <kevin123kevinl123@gmail.com>
2026-07-01 21:20:26 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
 };
+
 export default nextConfig;
