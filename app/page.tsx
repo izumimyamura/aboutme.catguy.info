12ff66aab3c8ddbe695bf152c71016709a2805be
THECATGUY <kevin123kevinl123@gmail.com>
2026-07-01 20:12:15 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,18 +9,18 @@ const { ScrollStackItem } = require('../components/ScrollStack');
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black text-white">
-      <nav className="fixed top-0 z-50 flex w-full items-center justify-between border-b border-white/10 bg-black/50 p-6 px-10 backdrop-blur-md">
-        <h1 className="text-xl font-bold">THE CAT GUY</h1>
-        <div className="space-x-6">
+    <main className="min-h-screen bg-black">
+      <nav className="fixed top-0 z-50 flex w-full items-center justify-between border-b border-white/10 bg-black/50 p-6 backdrop-blur-md">
+        <h1 className="text-xl font-bold text-white">THE CAT GUY</h1>
+        <div className="space-x-6 text-white">
           <a href="#hero">Home</a>
           <a href="https://kavin-portfolio-v2-4mowan065-catguy.vercel.app/#hero" target="_blank">Portfolio</a>
         </div>
       </nav>
 
-      <section id="hero" className="relative flex h-screen flex-col items-center justify-center overflow-hidden">
+      <section id="hero" className="relative flex h-screen flex-col items-center justify-center">
         <SideRays speed={2} rayColor1="#EAB308" rayColor2="#96c8ff" />
-        <BlurText text="THE CAT GUY" className="z-10 text-8xl font-black" />
+        <BlurText text="THE CAT GUY" className="z-10 text-8xl font-black text-white" />
       </section>
 
       <section className="px-10 py-20">
