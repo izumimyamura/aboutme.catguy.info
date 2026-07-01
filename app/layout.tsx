4a689cf0e39965b31e149b4a916debf2e21e8d07
THECATGUY <kevin123kevinl123@gmail.com>
2026-07-01 20:49:34 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white m-0 p-0">{children}</body>
+      <body className="bg-black text-white m-0 p-0 overflow-x-hidden">{children}</body>
     </html>
   );
 }
